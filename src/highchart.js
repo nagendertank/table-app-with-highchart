556c3f58e282079af4d0453dc23c787aae312543
Nagendra <nagendra.tank@citrix.com>
2020-06-05 14:42:18 +0530

    add change

---
+++ src/highchart.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { render } from "react-dom";
+import HighchartsReact from "highcharts-react-official";
+// Import Highcharts
+import Highcharts from "highcharts";
+import highchartsMore from "highcharts/highcharts-more";
+highchartsMore(Highcharts);
+
+
+export default class HighchartsComponent extends React.PureComponent {
+  constructor(props) {
+    console.log(props)
+    super(props);
+    this.afterChartCreated = this.afterChartCreated.bind(this);
+    this.state = {
+      chartOptions: {
+        chart: {
+            type: 'columnrange',
+            inverted: true
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            }
+        },
+
+        yAxis: {
+
+            labels: {
+                enabled: false
+            }
+        },
+        plotOptions: {
+            columnrange: {
+                dataLabels: {
+                    enabled: true,
+                    format: '{y}'
+                }
+            }
+        },
+    
+        series: [{
+            data: [
+               props.value
+            ]
+        }]
+      }
+    };
+  }
+
+  afterChartCreated(chart) {
+    this.internalChart = chart;
+    this.forceUpdate();
+  }
+
+  componentDidUpdate() {
+    //this.internalChart.getMargins(); // redraw
+    this.internalChart.reflow();
+  }
+
+  render() {
+    const chart = this.internalChart,
+      customLabels = [];
+
+
+    return (
+      <div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={this.state.chartOptions}
+          callback={this.afterChartCreated}
+        />
+        {customLabels}
+      </div>
+    );
+  }
+}
+
